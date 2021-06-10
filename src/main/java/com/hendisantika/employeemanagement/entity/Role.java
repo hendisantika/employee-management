a82f171a4d4ebc3798dc4e37511ea9c5bef2a124
Hendi Santika <hendisantika@gmail.com>
2021-06-10 09:02:39 +0700

    #6 - Add Role Entity

---
+++ src/main/java/com/hendisantika/employeemanagement/entity/Role.java
@@ -0,0 +1,32 @@
+package com.hendisantika.employeemanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : employee-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/06/21
+ * Time: 09.01
+ */
+@Entity
+@Table(name = "role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
