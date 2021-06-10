b6ea3b8ccd6cbfad6d1cb0011fe4b48ce2feaf32
Hendi Santika <hendisantika@gmail.com>
2021-06-10 09:26:44 +0700

    #28 - Add showRegistrationForm in UserRegistrationController

--- src/main/java/com/hendisantika/employeemanagement/controller/UserRegistrationController.java
+++ src/main/java/com/hendisantika/employeemanagement/controller/UserRegistrationController.java
@@ -3,6 +3,7 @@ package com.hendisantika.employeemanagement.controller;
 import com.hendisantika.employeemanagement.dto.UserRegistrationDto;
 import com.hendisantika.employeemanagement.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,5 +30,10 @@ public class UserRegistrationController {
     @ModelAttribute("user")
     public UserRegistrationDto userRegistrationDto() {
         return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
     }
 }
