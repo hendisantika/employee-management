30a94114d4454f085aae34274901c9f1020c2fc4
Hendi Santika <hendisantika@gmail.com>
2021-06-10 09:27:04 +0700

    #29 - Add registerUserAccount in UserRegistrationController

--- src/main/java/com/hendisantika/employeemanagement/controller/UserRegistrationController.java
+++ src/main/java/com/hendisantika/employeemanagement/controller/UserRegistrationController.java
@@ -5,6 +5,7 @@ import com.hendisantika.employeemanagement.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -35,5 +36,11 @@ public class UserRegistrationController {
     @GetMapping
     public String showRegistrationForm() {
         return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
     }
 }
