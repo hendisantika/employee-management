7a06d127fb612deccb303f22cb60fc2dd480ef5d
Hendi Santika <hendisantika@gmail.com>
2021-06-10 09:11:33 +0700

    #13 - Add getEmployeeById in EmployeeService

--- src/main/java/com/hendisantika/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/hendisantika/employeemanagement/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +29,16 @@ public class EmployeeService {
 
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return employee;
     }
 }
