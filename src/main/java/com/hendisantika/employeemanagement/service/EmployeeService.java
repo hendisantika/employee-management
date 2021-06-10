6ed505d5d501d07b4d1e4fb8b97ad0ff1602d50b
Hendi Santika <hendisantika@gmail.com>
2021-06-10 09:12:13 +0700

    #15 - Add findPaginated in EmployeeService

--- src/main/java/com/hendisantika/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/hendisantika/employeemanagement/service/EmployeeService.java
@@ -3,6 +3,10 @@ package com.hendisantika.employeemanagement.service;
 import com.hendisantika.employeemanagement.entity.Employee;
 import com.hendisantika.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,5 +48,13 @@ public class EmployeeService {
 
     public void deleteEmployeeById(long id) {
         this.employeeRepository.deleteById(id);
+    }
+
+    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.employeeRepository.findAll(pageable);
     }
 }
